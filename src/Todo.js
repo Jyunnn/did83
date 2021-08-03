44afa20a4704c95808127b624603351617f91b99
WQN <tel5430196@yahoo.com.tw>
2021-08-03 17:00:23 +0800

    hook重構 0803

--- src/Todo.js
+++ src/Todo.js
@@ -4,24 +4,18 @@ import TodoBox from './components/TodoBox'
 import './css/todo.css';
 
 function Todo() {
-  const [id, setId] = useState(0);
-  const [isfinish, setIsfinish] = useState(false);
-  const [todo, setTodo] = useState('');
-  const [hash , setHash] = useState(Date.now());
+  const [todo, setTodo] = useState({value:'', id: 0, hash: Date.now()});
   const [todolist, setTodolist] = useState([]);
-  const [finishlist, setFinishlist] = useState([]);
 
   const addTodo = (e) => {
     e.preventDefault();
-    if(todo === ''){
+    if(todo.value === ''){
       return
     }
     setTodolist ((old)=>{
-      return [...old, {id: id, hash: hash, finish: isfinish, todo: todo}]
-    })
-    setTodo('');
-    setHash(Date.now())
-    setId(id+1);
+      return [...old, todo]
+    });
+		setTodo({value:'',id: todo.id+=1, hash: Date.now()})
   }
 
   const delTodo = (e) => {
@@ -38,22 +32,7 @@ function Todo() {
 
   const handleFinishChange = (hash) => {
     let list = [...todolist];
-    let finishList = [...finishlist];
-
-    let arr = list.find((v)=>{
-      return v.hash === hash
-    })
-
-    let index = list.indexOf(arr);
-    list[index].finish = !list[index].finish
-
-
-    setTodolist (()=>{
-      return [...list]
-    })
-    setFinishlist(()=>{
-      return [...finishList]
-    })
+		console.log(hash, list);
   }
 
   const renderTodolist = (array) => {
@@ -61,35 +40,39 @@ function Todo() {
       return (
           <p> 空白 </p>
       )
-    } else {
-        return (
-          array.map((v)=>{ 
-            return (
-              <TodoBox key={v.hash} delTodo={delTodo}  finishChange={handleFinishChange} {...v}/>
-            ) 
-          })
-        )
-    }
+    }	else {
+			return (
+				array.map((v)=>{ 
+					return (
+						<TodoBox key={v.hash} delTodo={delTodo} handleFinishChange={handleFinishChange} {...v}/>
+					) 
+				})
+			)
+		}
+  }
+
+  const filiterTodos = () => {
+    return todolist.filter((v)=>{
+      return v.finish === true
+    })
   }
 
   return (
     <div className="App">
+			<form className="todo-form">
+				<input type="text" placeholder="請輸入文字" value={todo.value} onChange={(e)=>{setTodo({...todo, value: e.target.value })}}></input>
+				<button onClick={ addTodo }>
+					<img src={search} alt="search-icon"/>
+				</button>
+			</form>
 
-        <form className="todo-form">
-          <input type="text" placeholder="請輸入文字" value={todo} onChange={(e)=>{setTodo(e.target.value)}}></input>
-          <button onClick={ addTodo }>
-            <img src={search} alt="search-icon"/>
-          </button>
-        </form>
-
-        <div className="todolist-area">
-          {  renderTodolist(todolist) }
-        </div>
-        <div className="todolist-area">
-          {  renderTodolist(finishlist) }
-        </div>
-
-
+			<div className="todolist-area">
+				{  renderTodolist(todolist) }
+			</div>
+			---
+			<div className="todolist-area">
+				{  renderTodolist(filiterTodos())}
+			</div>
     </div>
   );
 }
--- src/components/TodoBox.js
+++ src/components/TodoBox.js
@@ -1,14 +1,22 @@
-function TodoBox(props) {
+import { useState } from 'react';
 
-    return (
-        <div className="todo">
-            <input type="checkbox" checked={props.isfinish} onChange={ ()=> props.finishChange(props.hash)} />
-            <p> 序列 : { props.id } </p>
-            <p> 時間戳 : { props.hash } </p>
-            <p> { props.todo } </p>
-            <button onClick={props.delTodo} value={ props.id }> DEL </button>
-        </div>
-    )
+function TodoBox(props) {
+	const [isfinish, setIsfinish] = useState(false);
+
+	const handleFinishChange = function (a) {
+		setIsfinish(!isfinish);
+		console.log(a);
+	}
+
+	return (
+		<div className="todo">
+			<input type="checkbox" checked={isfinish} onChange={()=>handleFinishChange(isfinish)} />
+			<p> 序列 : {props.id} </p>
+			<p> 時間戳 : {props.hash} </p>
+			<p> {props.value} </p>
+			<button onClick={props.delTodo} value={props.id}> DEL </button>
+		</div>
+	)	
 }
 
 export default TodoBox;
